945b25abd8d9d6b293a099dcc8e23c5d80f88c07
AbidShazid3 <abidshazid3@gmail.com>
2025-06-23 22:20:09 +0600

    env file added

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import { Server } from 'http';
 import app from './app';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 let server: Server;
 const port = 5000;
 
+const mongoUri = process.env.MONGO_URI as string;
+
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0.v0031py.mongodb.net/library-management-system?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect(mongoUri);
         console.log("Connected to MongoDB Using Mongoose!!");
         server = app.listen(port, () => {
             console.log(`App is listening on port ${port}`)
